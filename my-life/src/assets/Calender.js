4e0d2e3eb6eea61b0c38fb088423aa82c079d700
Josephmusngi21 <josephmusngi7@gmail.com>
2024-08-03 00:28:29 -0700

    w, updated calenderToList

--- my-life/src/assets/Calender.js
+++ my-life/src/assets/Calender.js
@@ -49,21 +49,29 @@ class Calender {
     }
 
     return output;
-  };
+  }
 
-  calenderToList(setMonth) {
+  calendarToList(setMonth) {
     let dayCount = this.months[setMonth][1];
     console.log(`dayCount: ${dayCount}`);
     let day = 1;
     let firstDay = new Date(this.date.getFullYear(), setMonth - 1, 1).getDay();
+    console.log(firstDay);
+    let calendar = [["Su", "M ", "T ", "W ", "Th", "F ", "Sa"]];
 
-    let calendar = [['Su', 'M ', 'T ', 'W ', 'Th', 'F ', 'Sa']];
-    while (day < dayCount) {
-        let week = [];
-        for (let i = firstDay; i < 7 && day <= dayCount[1]; i ++) {
-            week.push(Dates_())
-        };
-    };    
+    while (day <= dayCount) {
+      let week = [];
+      for (let i = 0; i < 7; i++) {
+        if (day > dayCount) break;
+        if (calendar.length === 1 && i < firstDay) {
+          week.push("   ");
+        } else {
+          week.push(day.toString().padStart(2, " "));
+          day++;
+        }
+      }
+      calendar.push(week);
+    }
 
     return calendar;
   }
@@ -73,4 +81,4 @@ module.exports = Calender;
 
 let c = new Calender();
 // console.log(c.createCalender(8));
-console.log(c.calenderToList(8));
+// console.log(c.calendarToList(8));
