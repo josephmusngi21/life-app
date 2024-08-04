51ff3209be4fa75b0749d0b2de408b9916162648
Josephmusngi21 <josephmusngi7@gmail.com>
2024-08-04 15:48:01 -0700

    p, still working on styling and creating notes

--- my-life/src/components/Main/Note/Note.jsx
+++ my-life/src/components/Main/Note/Note.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import style from '/Note.module.css';
+import NoteList from './NoteList/NoteList';
+import NoteShow from './NoteShow/NoteShow';
 
 function Note() {
   return (
     <div className={style.main}>
-      
+      <NoteList />
+      <NoteShow />
     </div>
   )
 }
