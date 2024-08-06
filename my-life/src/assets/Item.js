10c16507a2bf670456eaeb7799601dbbc0fa5d41
Josephmusngi21 <josephmusngi7@gmail.com>
2024-08-06 13:16:43 -0700

    p, initiated file

---
+++ my-life/src/assets/Item.js
@@ -0,0 +1,9 @@
+//This will be a script that will handle adding to lists and Tags,
+
+// Will allow them to also remove and add from the list, it will export a list of items
+
+class Item {
+
+}
+
+module.exports = Item;
